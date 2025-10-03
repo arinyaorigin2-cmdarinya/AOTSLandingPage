5f2fef22ccddfe898147a03507d51c87973a46aa
Navi7975 <naveen119993@gmail.com>
2025-10-03 18:46:45 +0530

    some deloyment changes

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import copy from "rollup-plugin-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    copy({
+      targets: [
+        { src: "public/_redirects", dest: "dist" }  // ✅ Copies _redirects to the build output
+      ],
+      hook: "writeBundle",
+    }),
   ],
 });
